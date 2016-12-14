9cbb092d94f1b5c93c2129f9baeadf63d46bede5
zhangxuchao <2212362272@qq.com>
2016-12-14 14:12:58 +0800

    Update npm.ts

--- npm.ts
+++ npm.ts
@@ -14,3 +14,26 @@ ionic2 serve
 
 5. 如果报以下错误： No component factory found for "组件名称" 
    因为在app.module.ts中entryComponents中没有注册组件，和ng2不同的地方就是，ng2中只需要在declarations总注册组件。
+
+6. 导航界面，传递参数， 单机导航，追加界面，并传递参数
+      goToOtherPage(id) {
+         this.navCtrl.push(OtherPage, {
+          id: id
+         });
+      }
+7. 获取传递过来的参数， 通过NavParams.get()方法
+   import {Component} from "@angular/core";
+   import {NavController, NavParams} from "ionic-angular";
+   @Component({
+     templateUrl: "otherPage.html"
+   })
+   export class OtherPage {
+     id: String;
+     constructor(public navCtrl: NavController, private navParams: NavParams) {
+       // alert(navParams.get("id"));
+       this.id = navParams.get("id");
+     }
+     goBack() {
+       this.navCtrl.pop();
+     }
+   }
