952d1b600ad75e992bf89f0181bf2e9e440b2344
zhangxuchao <2212362272@qq.com>
2016-12-14 13:09:41 +0800

    Update npm.ts

--- npm.ts
+++ npm.ts
@@ -12,4 +12,5 @@ ionic2 serve
 3. 新添加的组件，服务，指令，管道，都要添加到 app/app.module.ts中，和ng2是一样的。
 4. 组件中的图片放置在目录 根目录/www/assets/img文件夹中   通过<img src="assets/img/favicon.ico" />引用
 
-5. 如果报以下错误： No component factory found for "组件名称" 因为在app.module.ts中entryComponents中没有注册组件，和ng2不同的地方就是，ng2中只需要在declarations总注册组件。
+5. 如果报以下错误： No component factory found for "组件名称" 
+   因为在app.module.ts中entryComponents中没有注册组件，和ng2不同的地方就是，ng2中只需要在declarations总注册组件。
