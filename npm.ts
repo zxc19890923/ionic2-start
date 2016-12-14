8605da707ac26b12d433c10d75b50863959f8b47
zhangxuchao <2212362272@qq.com>
2016-12-14 11:18:37 +0800

    Update npm.ts

--- npm.ts
+++ npm.ts
@@ -9,4 +9,5 @@ ionic2 serve
 1. ionic serve 启动服务 浏览器浏览
 2. ionic build ios/android 创建ios和android应用
 
-新添加的组件，服务，指令，管道，都要添加到 app/app.module.ts中，和ng2是一样的。
+3. 新添加的组件，服务，指令，管道，都要添加到 app/app.module.ts中，和ng2是一样的。
+4. 组件中的图片放置在目录 根目录/www/assets/img文件夹中   通过<img src="assets/img/favicon.ico" />引用
