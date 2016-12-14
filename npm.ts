21d71b1c286b10f33ed8d2bf26302e398e852de4
zhangxuchao <2212362272@qq.com>
2016-12-14 09:27:09 +0800

    Update npm.ts

--- npm.ts
+++ npm.ts
@@ -1,2 +1,10 @@
+// 安装cordova
+npm install -g ionic cordova  
+$ ionic start ionic2-test --v2  
+// 安装 ionic2-test
+cd ionic2
+// 启动
+ionic2 serve
+
 1. ionic serve 启动服务 浏览器浏览
 2. ionic build ios/android 创建ios和android应用
